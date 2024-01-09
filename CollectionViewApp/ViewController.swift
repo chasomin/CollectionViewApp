244e2324d1e34ff950a196dee3e1f2b767e7c14e
chasomin <somin49@naver.com>
2024-01-09 16:40:52 +0900

    Initial Commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by 차소민 on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
