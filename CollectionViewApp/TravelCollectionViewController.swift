d7916c798c3627faa15c50a6401976995a2578ac
chasomin <somin49@naver.com>
2024-01-09 17:37:45 +0900

    collectionViewController(+XIB)

---
+++ CollectionViewApp/TravelCollectionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TravelCollectionViewController.swift
+//  CollectionViewApp
+//
+//  Created by 차소민 on 1/9/24.
+//
+
+import UIKit
+
+enum Identifier: String {
+    case TravelCollectionViewCell
+    case TravelXIBCollectionViewCell
+}
+
+class TravelCollectionViewController: UICollectionViewController {
+
+    let cityList = CityInfo().city
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let xib = UINib(nibName: Identifier.TravelXIBCollectionViewCell.rawValue, bundle: nil)
+        collectionView.register(xib, forCellWithReuseIdentifier: Identifier.TravelXIBCollectionViewCell.rawValue)
+
+        
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 20
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        
+        layout.itemSize = CGSize(width: width/2, height: width/2 + 70)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        collectionView.collectionViewLayout = layout
+
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cityList.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifier.TravelXIBCollectionViewCell.rawValue, for: indexPath) as! TravelXIBCollectionViewCell
+        
+        cell.configureCell(city: cityList[indexPath.item])
+        
+        return cell
+    }
+
+}
---
+++ CollectionViewApp/XIB/TravelXIBCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TravelXIBCollectionViewCell.swift
+//  CollectionViewApp
+//
+//  Created by 차소민 on 1/9/24.
+//
+
+import UIKit
+
+class TravelXIBCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var cityImageView: UIImageView!
+    @IBOutlet var cityName: UILabel!
+    @IBOutlet var cityExplain: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        cityImageView.layer.cornerRadius = ((UIScreen.main.bounds.width - (20 * 3)) / 2) / 2
+        cityImageView.contentMode = .scaleAspectFill
+        
+        cityName.font = .boldSystemFont(ofSize: 15)
+        cityName.textAlignment = .center
+        
+        cityExplain.font = .systemFont(ofSize: 13)
+        cityExplain.textColor = .lightGray
+        cityExplain.textAlignment = .center
+        cityExplain.numberOfLines = 0
+        
+    }
+    
+    func configureCell(city: City) {
+        cityImageView.kf.setImage(with: URL(string: city.city_image))
+        
+        cityName.text = "\(city.city_name) | \(city.city_english_name)"
+        cityExplain.text = city.city_explain
+        
+    }
+   
+}
