f8e97bf4425b246e15b351490d5c5a001e2be729
chasomin <somin49@naver.com>
2024-01-11 16:45:59 +0900

    뷰컨, 셀 파일 추가

---
+++ CollectionViewApp/Cell/CityTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CityTableViewCell.swift
+//  CollectionViewApp
+//
+//  Created by 차소민 on 1/11/24.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    static let id = "CityTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+    
+}
---
+++ CollectionViewApp/CityList/CityListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CityListViewController.swift
+//  CollectionViewApp
+//
+//  Created by 차소민 on 1/11/24.
+//
+
+import UIKit
+
+class CityListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
